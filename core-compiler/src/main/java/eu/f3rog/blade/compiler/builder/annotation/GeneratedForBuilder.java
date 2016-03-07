2424ae7ec9c4b96397a7500014a4a4017c78fc2e
FrantisekGazo <frantisek.gazo@inloop.eu>
2016-03-07 22:43:44 +0100

    Added @GeneratedFor in blade.I class. Fixed mvp module

---
+++ core-compiler/src/main/java/eu/f3rog/blade/compiler/builder/annotation/GeneratedForBuilder.java
@@ -0,0 +1,27 @@
+package eu.f3rog.blade.compiler.builder.annotation;
+
+import com.squareup.javapoet.AnnotationSpec;
+import com.squareup.javapoet.ClassName;
+
+import eu.f3rog.blade.core.GeneratedFor;
+
+/**
+ * Class {@link GeneratedForBuilder}
+ *
+ * @author FrantisekGazo
+ * @version 2016-03-07
+ */
+public class GeneratedForBuilder {
+
+    public static AnnotationSpec buildFor(Class cls) {
+        return AnnotationSpec.builder(GeneratedFor.class)
+                .addMember("value", "$T.class", cls)
+                .build();
+    }
+
+    public static AnnotationSpec buildFor(ClassName className) {
+        return AnnotationSpec.builder(GeneratedFor.class)
+                .addMember("value", "$T.class", className)
+                .build();
+    }
+}
