35c7ae9167711822193e75bdad29183e7891cc93
FrantisekGazo <frantisek.gazo@inloop.eu>
2016-04-06 15:30:03 +0200

    Added documentation for @Parcel and @ParcelIgnore.

--- module/parcel/src/main/java/blade/ParcelIgnore.java
+++ module/parcel/src/main/java/blade/ParcelIgnore.java
@@ -6,6 +6,12 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.SOURCE;
 
+/**
+ * If field is annotated with @{@link ParcelIgnore}
+ * then the generated {@link android.os.Parcelable} implementation will ignore this field.
+ *
+ * @author FrantisekGazo
+ */
 @Target(FIELD)
 @Retention(SOURCE)
 public @interface ParcelIgnore {
