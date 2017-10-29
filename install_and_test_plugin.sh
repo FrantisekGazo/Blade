cee9096b46129185f88c4dd532824cfcb6a5f195
František Gažo <frantisek.gazo.313@gmail.com>
2017-10-29 12:20:44 +0100

    Add a script for local plugin testing"

---
+++ install_and_test_plugin.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -Rf ~/Library/Android/sdk/extras/android/m2repository/eu/
+rm -Rf ~/Library/Android/sdk/extras/android/m2repository/eu/
+./gradlew clean install
+mkdir ~/Library/Android/sdk/extras/android/m2repository/eu/
+cp -R ~/.m2/repository/eu/ ~/Library/Android/sdk/extras/android/m2repository/eu/
+
+./gradlew :plugin:test
